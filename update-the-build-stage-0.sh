8e299065b2b9d380927f84c113d417867e83af9a
William Stein <wstein@sagemath.com>
2018-11-09 11:33:36 -0800

    rebuild cocalc exactly when it changes

--- update-the-build-stage-0.sh
+++ update-the-build-stage-0.sh
@@ -3,6 +3,6 @@ sudo docker stop cocalc-test
 sudo docker rm cocalc-test
 set -e
 git pull
-time sudo docker build --build-arg commit=$1 -t cocalc .
+time sudo docker build --build-arg commit=`git ls-remote -h https://github.com/sagemathinc/cocalc master | awk '{print $1}'` -t cocalc .
 sudo docker tag cocalc:latest sagemathinc/cocalc
 sudo docker run --name=cocalc-test -d -v ~/cocalc-test:/projects -p 4043:443 sagemathinc/cocalc
